8b8bca55fad570403afb26e33a8b6bec57eef96f
sahilhedau49 <hedausahil49@gmail.com>
2023-05-14 12:15:11 +0530

    Registration Page Responsive Done

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    theme: {
+      screens: {
+        sm: "576px",
+        // => @media (min-width: 576px) { ... }
+
+        md: "960px",
+        // => @media (min-width: 960px) { ... }
+
+        lg: "1440px",
+        // => @media (min-width: 1440px) { ... }
+      },
+    },
   },
   plugins: [],
 };
